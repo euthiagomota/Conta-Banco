b1f6f4026dbd8edbc9d9fa793e8d5a3b41913d5b
Thiago Mota <euthiagojose2004@gmail.com>
2024-08-01 10:39:38 -0300

    tratamento de erro e exceções

--- src/ContaService.java
+++ src/ContaService.java
@@ -1,4 +1,3 @@
-import javax.xml.namespace.QName;
 import java.util.Scanner;
 
 public class ContaService {
@@ -6,30 +5,37 @@ public class ContaService {
     public static void criarConta() {
         Scanner scanner = new Scanner(System.in);
 
-try {
-    System.out.println("Bem-vindo(a) ao nosso banco, por favor informe seu nome: ");
-    String name = scanner.nextLine();
+        try {
+            System.out.println("Bem-vindo(a) ao nosso banco, por favor informe seu nome: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                System.out.println("O nome não pode ser vazio.");
+                criarConta();
+            } else {
+                System.out.println("Agora digite sua agencia: ");
+                String agencia = scanner.nextLine();
+                if (agencia.isEmpty()) {
+                    System.out.println("A agencia não deve ser vazia.");
+                } else {
+                    System.out.println("Digite o numero da conta: ");
+                    int numero = scanner.nextInt();
 
-    System.out.println("Agora digite sua agencia: ");
-    String agencia = scanner.nextLine();
+                    System.out.println("Digite seu saldo atual: ");
+                    double saldo = scanner.nextDouble();
 
-    System.out.println("Digite o numero da conta: ");
-    int numero = scanner.nextInt();
+                    ContaModel contaModel = new ContaModel(agencia, saldo, numero, name);
 
-    System.out.println("Digite seu saldo atual: ");
-    double saldo = scanner.nextDouble();
+                    System.out.println("Conta criada com sucesso! " + "\n" +
+                            "Nome: " + contaModel.getNome() + "\n" +
+                            "Agência: " + contaModel.getAgencia() + "\n" +
+                            "Numero: " + contaModel.getNumero() + "\n" +
+                            "Saldo: " + contaModel.getSaldo());
+                }
+            }
 
-    ContaModel contaModel = new ContaModel(agencia, saldo, numero, name);
-
-    System.out.println("Conta criada com sucesso! " + "\n" +
-            "Nome:" + contaModel.getNome() + "\n" +
-            "Agência: " + contaModel.getAgencia() + "\n" +
-            "Numero: " + contaModel.getNumero() + "\n" +
-            "Saldo: " + contaModel.getSaldo());
-} catch (Exception exception) {
-    System.out.println("Ops! Algo deu errado na criação da sua conta, tente novamente.");
-    criarConta();
-}
-
+        } catch (Exception exception) {
+            System.out.println("Ops! Algo deu errado na criação da sua conta, tente novamente.");
+            criarConta();
+        }
     }
 }
